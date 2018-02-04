a711a5e51fd4691c44c35b1e5960551f97c2b3bc
akwicks1 <akwicks1@gmail.com>
2018-02-03 18:52:48 -0800

    linking the server to the db

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
-#from model import connect_to_db, db, User, Favorite, Dog, Shelter, Breed
+from model import connect_to_db, db, User, Resident, GarbageCollector
 
 import os
 
